bd4a0d5af87b6b02c5a4349263465e2ced9d46e2
prismaplatform <101548244+prismaplatform@users.noreply.github.com>
2025-12-09 22:56:55 +0200

    Change domain from 'leiterkurierat' to 'leiterkuriercom'

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -58,7 +58,7 @@ export default async function RootLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const domain = "leiterkurierat";
+  const domain = "leiterkuriercom";
   
   const cookieStore = cookies();
 
